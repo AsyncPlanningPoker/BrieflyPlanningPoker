5dbfd2c3f64296add9266ea6cf52db0b253e96e0
lineconquista <lineconquista2010@hotmail.com>
2022-03-19 08:08:04 -0300

    :wrench: add dockerfile and docker-compose config

--- packages/back/api/src/index.ts
+++ packages/back/api/src/index.ts
@@ -5,7 +5,7 @@ app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 if (require.main === module) {
   app.listen(port, () => {
     return console.log(`BrieflyPlanningPoker app listening at ${port} port`);
