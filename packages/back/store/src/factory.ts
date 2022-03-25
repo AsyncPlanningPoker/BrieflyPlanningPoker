e583e6c62dd897b78b92266962007565446059fc
lineconquista <lineconquista2010@hotmail.com>
2022-03-24 21:34:31 -0300

    :sparkles: add factory design pattern

---
+++ packages/back/store/src/factory.ts
@@ -0,0 +1,15 @@
+import { UserDbStore } from './stores/user';
+import knex from 'knex';
+import knexfile from '../knexfile';
+
+interface FactoryStoreResult {
+  userDbStore: UserDbStore;
+}
+
+async function createStores(): Promise<FactoryStoreResult> {
+  const client = knex(knexfile);
+  const userDbStore = new UserDbStore(client);
+  return { userDbStore };
+}
+
+export { createStores };
