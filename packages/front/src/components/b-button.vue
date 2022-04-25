015cf1732eb175d20b1ccc0b72a1a5132965fdef
lulcca <lucca.jacomassi@hotmail.com>
2022-04-25 00:45:14 -0300

    :sparkles: new components: button, container and input

---
+++ packages/front/src/components/b-button.vue
@@ -0,0 +1,56 @@
+<template>
+  <button
+    class="b-button"
+    :class="{'b-button--small': size === 'small'}"
+    :type="type"
+  >
+    {{ this.value.toUpperCase() }}
+  </button>
+</template>
+
+<script>
+import { shouldBeOneOf } from 'vue-prop-validation-helper';
+
+export default {
+  name: 'BButton',
+  props: {
+    size: {
+      type: String,
+      default: 'medium',
+      validator: shouldBeOneOf(['small', 'medium']),
+    },
+    type: {
+      type: String,
+      default: 'button',
+      validator: shouldBeOneOf(['button', 'reset', 'submit']),
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.b-button {
+  background-color: var(--color-primary);
+  border: none;
+  border-radius: var(--unit-0100);
+  color: var(--color-white);
+  font-family: 'Hammersmith One', sans-serif;
+  font-size: var(--unit-0500);
+  height: var(--unit-1000);
+  padding: var(--unit-0100);
+  width: inherit;
+
+  &:hover{
+    background-color: var(--color-accent);
+    cursor: pointer;
+  }
+}
+
+.b-button--small {
+  font-size: var(--unit-0400);
+}
+</style>
---
+++ packages/front/src/components/b-input.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="b-input">
+    <div class="b-input__label">
+      <label :for="id"> 
+        {{ this.label }} 
+      </label>
+      <a v-if="hasLink" :href="this.link">
+        {{ this.linkLabel }}
+      </a>
+    </div>
+    
+    <div class="b-input__container">
+      <input
+        class="b-input__field"
+        :id="id"
+        :placeholder="placeholder"
+        :type="type"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { shouldBeOneOf } from 'vue-prop-validation-helper';
+
+export default {
+  name: 'BInput',
+  props: {
+    id: {
+      type: String,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+    },
+    linkLabel: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: 'text',
+      validator: shouldBeOneOf(['text','email','password']),
+    },
+    placeholder: {
+      type: String,
+    },
+  },
+  computed: {
+    hasLink() {
+      return this.link;
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.b-input {
+  font-family: 'Hammersmith One', sans-serif;
+  font-size: var(--unit-0400);
+}
+
+.b-input__container {
+  align-items: center;
+  background-color: var(--color-white);
+  border-radius: var(--unit-0100);
+  display: flex;
+  height: var(--unit-1000);
+  justify-content: start;
+  margin-top: var(--unit-0200);
+}
+
+.b-input__field {
+  background-color: transparent;
+  border: none;
+  font-family: inherit;
+  font-size: inherit;
+  padding: var(--unit-0200);
+  width: 100%;
+}
+
+.b-input__label {
+  display: flex;
+  font-family: inherit;
+  font-size: inherit;
+  justify-content: space-between;
+}
+</style>
