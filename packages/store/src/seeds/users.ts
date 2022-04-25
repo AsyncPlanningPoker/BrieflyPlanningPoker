969e104b9833fc7097e64e218872b78be9803fb5
lineconquista <lineconquista2010@hotmail.com>
2022-04-25 10:07:48 -0300

    :seedling: update users seed

--- packages/store/src/seeds/users.ts
+++ packages/store/src/seeds/users.ts
@@ -9,8 +9,8 @@ type IUserSeed = {
 
 export async function seed(knex: Knex): Promise<void> {
   const users: IUserSeed[] = [
-    { id: '715cadc0-bae2-453b-97cf-d5a54d8f2c86', name: 'Line Conquista', email: 'line@briefly.com', password: 'line12' },
-    { id: '7e13d8f9-159e-4bfb-b67f-1f9cd3084813', name: 'Lucca Jacomassi', email: 'lulcca@briefly.com', password: 'lulcca12' },
+    { id: '715cadc0-bae2-453b-97cf-d5a54d8f2c86', name: 'Line Conquista', email: 'line@briefly.com', password: '$2a$10$IrVhCnXDAxEhVknwQlI/IONAJZwDXRSSzRt5Yb.n46CXVxj27jFSq' },
+    { id: '7e13d8f9-159e-4bfb-b67f-1f9cd3084813', name: 'Lucca Jacomassi', email: 'lulcca@briefly.com', password: '$2a$10$QBf6GkMWsMh8X3PpRC7Kbu9JDxTVwH3xdo2NxumJWSbsgGocUCntm' },
   ];
 
   await knex('users').del();
