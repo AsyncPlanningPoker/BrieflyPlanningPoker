2021f2c313f04235385a015dd4a1921d2ce006cf
lulcca <lucca.jacomassi@hotmail.com>
2022-04-26 03:04:43 -0300

    :sparkles: new component: brand

---
+++ packages/front/src/components/b-brand.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="b-brand__container">
+    <div class="b-brand__logo-one">
+      <img class="b-brand__image" src='../assets/brand-icon-circle.png' alt="brand-logo-circle">
+    </div>
+    <div class="b-brand__logo-two">
+      <img class="b-brand__image" src="../assets/brand-icon-rectangle.png" alt="brand-logo-rectangle">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BBrand',
+};
+</script>
+
+<style scoped lang="scss">
+.b-brand__container {
+  display: grid;
+  row-gap: var(--unit-0300);
+  justify-items: center;
+}
+
+.b-brand__logo-one {
+  height: 57px;
+  width: 57px;
+}
+
+.b-brand__logo-two {
+  height: 57px;
+  width: 172px;
+}
+
+.brand__image {
+  height: 100%;
+  width: 100%; 
+}
+</style>
