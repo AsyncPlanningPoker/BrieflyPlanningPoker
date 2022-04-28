0c7d87a231ee52c182d43a0046bbf4b8e592d2f9
lineconquista <lineconquista2010@hotmail.com>
2022-04-27 23:15:02 -0300

    :lipstick: add selection color

--- packages/front/src/App.vue
+++ packages/front/src/App.vue
@@ -3,22 +3,13 @@
 </template>
 
 <script>
-import SignIn from './pages/sign-in.vue';
 
 export default {
   name: 'App',
-  components: {
-    SignIn,
-  }
 }
+
 </script>
 
 <style lang="scss">
-  @import './styles/global.scss';
-
-  body{
-    margin: 0;
-    font-family: 'Hammersmith One', sans-serif;
-  }
-  
+  @import './styles/global.scss';  
 </style>
