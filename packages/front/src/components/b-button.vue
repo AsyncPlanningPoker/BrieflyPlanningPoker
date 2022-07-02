d67b4ce17bb80ae9455301c61f85ca2586bd71d7
lulcca <lucca.jacomassi@hotmail.com>
2022-07-02 19:57:47 -0300

    :zap: vee-validate everywhere

--- packages/front/src/components/b-button.vue
+++ packages/front/src/components/b-button.vue
@@ -1,7 +1,10 @@
 <template>
   <button
     class="b-button"
-    :class="{'b-button--small': size === 'small'}"
+    :class="{
+      'b-button--small': size === 'small',
+      'b-button--transparent': transparent
+    }"
     :type="type"
   >
     {{ this.value.toUpperCase() }}
@@ -13,11 +16,16 @@ import { shouldBeOneOf } from 'vue-prop-validation-helper';
 
 export default {
   name: 'BButton',
+
   props: {
     size: {
       type: String,
       default: 'medium',
       validator: shouldBeOneOf(['small', 'medium']),
+    },
+    transparent: {
+      type: Boolean,
+      default: false,
     },
     type: {
       type: String,
@@ -44,7 +52,8 @@ export default {
   padding: var(--unit-0100);
   width: inherit;
 
-  &:hover{
+  &:hover,
+  &:focus {
     background-color: var(--color-accent);
     cursor: pointer;
   }
@@ -52,5 +61,58 @@ export default {
 
 .b-button--small {
   font-size: var(--unit-0400);
+}
+
+.b-button--transparent {
+  background-color: transparent;
+  border: var(--unit-0050) solid var(--color-white);
+
+  &:hover,
+  &:focus {
+    background-color: transparent;
+    border-color: var(--color-accent);
+    color: var(--color-accent);
+  }
+}
+
+.invalid {
+  animation: shake 0.5s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  0% {
+    transform: translate(1px, 1px);
+  }
+  10% {
+    transform: translate(-1px, -2px);
+  }
+  20% {
+    transform: translate(-3px, 0px);
+  }
+  30% {
+    transform: translate(3px, 2px);
+  }
+  40% {
+    transform: translate(1px, -1px);
+  }
+  50% {
+    transform: translate(-1px, 2px);
+  }
+  60% {
+    transform: translate(-3px, 1px);
+  }
+  70% {
+    transform: translate(3px, 1px);
+  }
+  80% {
+    transform: translate(-1px, -1px);
+  }
+  90% {
+    transform: translate(1px, 2px);
+  }
+  100% {
+    transform: translate(1px, -2px);
+  }
 }
 </style>
--- packages/front/src/pages/sign-up.vue
+++ packages/front/src/pages/sign-up.vue
@@ -1,115 +1,152 @@
 <template>
   <div class="sign-up">
     <BBrand/>
+
     <BContainer>
-      <form class="sign-up__form">
-        <BInput
-          class="sign-up__label"
+      <Form
+        class="sign-up__form"
+        :validation-schema="schema"
+        @submit="onSubmit"
+        @invalid-submit="onInvalidSubmit"
+      >
+        <BInputField
           label="Name"
-          type="text"
-          @input="updateName"
-        />
-        <BInput
-          class="sign-up__label"
+          name="name"
+        >
+          <BInput
+            name="name"
+            type="text"
+            @input="updateName"
+          />
+        </BInputField>
+
+        <BInputField
           label="E-mail"
-          type="email"
-          @input="updateEmail"
-        />
-        <BInput
-          class="sign-up__label"
+          name="email"
+        >
+          <BInput
+            name="email"
+            type="email"
+            @input="updateEmail"
+          />
+        </BInputField>
+
+        <BInputField
           label="Password"
-          type="password"
-          @input="updatePassword"
-        />
-        <BInput
-          class="sign-up__label"
+          name="password"
+        >
+          <BInput
+            name="password"
+            type="password"
+            @input="updatePassword"
+          />
+        </BInputField>
+
+        <BInputField
           label="Confirm password"
-          type="password"
-          @input="updateConfirmPassword"
-          @keyup.enter="registry"
-        />
-        <div class="sign-up__label error">
+          name="confirmPassword"
+        >
+          <BInput
+            name="confirmPassword"
+            type="password"
+            @input="updateConfirmPassword"
+          />
+        </BInputField>
+
+        <BText
+          class="error"
+          size="small"
+          tag="div"
+        >
           {{ this.$store.state.signUp.errorMessage }}
-        </div>
+        </BText>
+
         <div class="sign-up__buttons-container">
-          <a href="/signin">
-            <BButton
-              value="return"
-            />
-          </a>
-          <BButton 
-            type="button" 
+          <BButton
+            :transparent="true"
+            value="return"
+            @click="$router.push('signin')"
+          />
+          
+          <BButton
+            class="sign-up__create-button"
+            type="submit"
             value="create"
-            @click="registry"
           />
         </div>
       </form>
     </BContainer>
-    <div class="sign-up__terms">
+
+    <BText 
+      class="sign-up__terms"
+      size="small"
+      tag="p"
+    >
       By creating an account, you agree to the <a href="#">Terms of Service</a>.
-    </div>
+    </BText>
   </div>
 </template>
 
 <script>
-import BBrand from './../components/b-brand.vue'
-import BButton from './../components/b-button.vue'
-import BContainer from './../components/b-container.vue'
-import BInput from './../components/b-input.vue';
+import { Form } from "vee-validate";
+import * as Yup from "yup";
+import BBrand from '../components/b-brand.vue'
+import BButton from '../components/b-button.vue'
+import BContainer from '../components/b-container.vue'
+import BInput from '../components/b-input.vue';
+import BInputField from '../components/b-input-field.vue';
+import BText from '../components/b-text.vue';
+import SignUp from '../store';
 
 export default {
   name: 'SignUp',
+
   components: {
     BBrand,
     BButton,
     BContainer,
     BInput,
+    BInputField,
+    BText,
+    Form,
   },
-  methods: {
-    updateName (e) {
-      this.$store.commit('updateName', e.target.value)
-      this.$store.commit('updateErrorMessage', '')
-    },
-    updateEmail (e) {
-      this.$store.commit('updateEmail', e.target.value)
-      this.$store.commit('updateErrorMessage', '')
-    },
-    updatePassword (e) {
-      this.$store.commit('updatePassword', e.target.value)
-      this.$store.commit('updateErrorMessage', '')
-    },
-    updateConfirmPassword (e) {
-      this.$store.commit('updateConfirmPassword', e.target.value)
-      this.$store.commit('updateErrorMessage', '')
-    },
-    registry(){
-      const name = this.$store.state.signUp.name
-      const email = this.$store.state.signUp.email
-      const password = this.$store.state.signUp.password
-      const confirmPassword = this.$store.state.signUp.confirmPassword
 
-      if(!name){
-        this.$store.commit('updateErrorMessage', "name is required")
-      }
-      else if(!email){
-        this.$store.commit('updateErrorMessage', "email is required")
-      }
-      else if(!new RegExp('[a-z0-9]+(([.]|[-]|[_])[a-z0-9]+)?@[a-z]+([.][a-z]{2,3})+').test(email)){
-        this.$store.commit('updateErrorMessage', "email is invalid")
-      }
-      else if(!password){
-        this.$store.commit('updateErrorMessage', "password is required")
-      }
-      else if(password.length < 6){
-        this.$store.commit('updateErrorMessage', "password must have at least 6 characters")
-      }
-      else if(password !== confirmPassword ){
-        this.$store.commit('updateErrorMessage', "passwords do not match")
-      }
-      else {
-        this.$store.dispatch('registry')
-      }
-    },
+  setup() {
+    function onSubmit() {
+      SignUp.dispatch('registry');
+    };
+
+    function onInvalidSubmit() {
+      const submitButton = document.querySelector(".sign-up__create-button");
+
+      submitButton.classList.add("invalid");
+      setTimeout(() => { submitButton.classList.remove("invalid"); }, 1000);
+    };
+
+    function updateName (e) {
+      SignUp.commit('updateName', e.target.value);
+    };
+
+    function updateEmail(e) {
+      SignUp.commit('updateEmail', e.target.value);
+    };
+
+    function updatePassword(e) {
+      SignUp.commit('updatePassword', e.target.value);
+    };
+
+    function updateConfirmPassword (e) {
+      SignUp.commit('updateConfirmPassword', e.target.value);
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      confirmPassword: Yup.string().oneOf([Yup.ref("password")], "passwords do not match"),
+    });
+
+    return { onSubmit, onInvalidSubmit, updateName, updateEmail, updatePassword, updateConfirmPassword, schema };
   },
 }
 </script>
@@ -125,11 +162,10 @@ export default {
 }
 
 .sign-up__form {
+  display: grid;
+  margin-top: calc(var(--unit-0200) * -1);
+  row-gap: var(--unit-0200);
   width: 280px;
-}
-
-.sign-up__label {
-  margin-top: var(--unit-0600);
 }
 
 .sign-up__buttons-container {
@@ -140,13 +176,6 @@ export default {
 
 .sign-up__terms {
   color: var(--color-white);
-  font-size: var(--unit-0300);
   margin-top: var(--unit-0200);
-}
-
-.error {
-  color: var(--color-error);
-  justify-self: center;
-  min-height: var(--unit-0500);
 }
 </style>
