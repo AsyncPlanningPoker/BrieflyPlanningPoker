15c10679f8ba3a64366a5575a30ab3e776989fed
lineconquista <lineconquista2010@hotmail.com>
2022-09-27 02:32:03 -0300

    aaa 44

---
+++ teste.js
@@ -0,0 +1,14 @@
+const dotenv = require ('dotenv');
+const execSync= require('child_process');
+
+dotenv.config({ path: `.env`});
+
+function setScriptProduction (){
+    if(process.env.PROD_ENV==='front'){
+        execSync.execSync('cd packages/front && yarn build')
+    }else if (process.env.PROD_ENV==='back'){
+        execSync.execSync('cd packages/api && yarn build')
+    }
+}
+
+setScriptProduction()
