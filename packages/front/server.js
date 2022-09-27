d639fb934aeb4bc4a7ec8a587a062ad8bd1a974d
lineconquista <lineconquista2010@hotmail.com>
2022-09-26 22:47:56 -0300

    ajusteee

--- packages/front/server.js
+++ packages/front/server.js
@@ -9,4 +9,6 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
+console.log(8)
+console.log(process.env.PORT)
 app.listen(process.env.PORT || 8080);
