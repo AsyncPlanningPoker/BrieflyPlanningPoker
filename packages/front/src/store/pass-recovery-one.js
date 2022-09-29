cd05c7eaaef0d28c9544f1d17b4a589976ef1e5e
lineconquista <lineconquista2010@hotmail.com>
2022-09-29 03:24:44 -0300

    :bug: redirect to signin after pass recovery

--- packages/front/src/store/pass-recovery-one.js
+++ packages/front/src/store/pass-recovery-one.js
@@ -23,7 +23,7 @@ const passRecoveryOneStore = {
       api
         .post('user/pass-recovery', { email: this.state.passRecoveryOne.email, url: 'http://localhost:8080/confirm_reset?token=' })
         .then(() => {
-          router.push('/');
+          router.push('/signin');
         })
         .catch((err) => {
           commit('updateErrorMessage', err.response.data.message);
