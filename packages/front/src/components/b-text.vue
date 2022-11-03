89f061ca4498a1d1bdb5e5e02fa9a1a7f73f062d
lulcca <lucca.jacomassi@hotmail.com>
2022-11-02 23:08:48 -0300

    commit criativo final 2

--- packages/front/src/components/b-text.vue
+++ packages/front/src/components/b-text.vue
@@ -26,7 +26,7 @@ export default {
     },
     color: {
       type: String,
-      default: 'white',
+      default: undefined,
       validator: shouldBeOneOf(['primary', 'accent', 'white', 'gray-10', 'gray-20', 'gray-30', 'black', 'link', 'error']),
     },
     size: {
