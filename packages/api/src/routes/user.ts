3c5d9240132af7d57df549f82c108cc37f899205
lulcca <lucca.jacomassi@hotmail.com>
2022-11-30 23:43:16 -0300

    :adhesive_bandage: enable back sending email when recovering password

--- packages/api/src/routes/user.ts
+++ packages/api/src/routes/user.ts
@@ -1,7 +1,8 @@
 import { CustomError, Unauthorized } from '../middlewares/error/error';
 import * as auth from '../middlewares/authorization/authorization';
-import { NextFunction, Request, Response } from 'express';
+import send from '../services/email';
 import * as crypt from '../utils/crypt';
+import { NextFunction, Request, Response } from 'express';
 import { randomUUID } from 'crypto';
 
 async function create(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
@@ -53,14 +54,13 @@ async function passRecovery(req: Request, res: Response, next: NextFunction): Pr
   const db = req.app.get('userDbStore');
   const user = await db.findByEmail(email);
   const token = user ? auth.create(email, 'pass-recovery', 300) : auth.create('inexistentAccount', 'inexistentAccount', 0);
-  return res.status(200).json({});
-  // await send({ to: email, subject: 'password recovery', message: `${req.body.url}${token}` })
-  // .then(() => {
-  //   return res.status(200).json({});
-  // })
-  // .catch((error: any) => {
-  //   return next(error);
-  // });
+  await send({ to: email, subject: 'BRIEFLY - Password Recovery', message: `Hey, did you ask for a password recovery?\n\nThis is your link ${req.body.url}${token}` })
+    .then(() => {
+      return res.status(200).json({});
+    })
+    .catch((error: any) => {
+      return next(error);
+    });
 }
 
 async function passUpdate(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
@@ -85,8 +85,8 @@ async function deleteUser(req: Request, res: Response, next: NextFunction): Prom
   const db = req.app.get('userDbStore');
 
   try {
-      await db.deleteByEmail(req.query.user, { updatedAt: new Date() });
-      return res.sendStatus(200);
+    await db.deleteByEmail(req.query.user, { updatedAt: new Date() });
+    return res.sendStatus(200);
   } catch (error: any) {
     next(error);
   }
@@ -96,23 +96,21 @@ async function updateUser(req: Request, res: Response, next: NextFunction): Prom
   const db = req.app.get('userDbStore');
   const email = req.query.user;
 
-
   try {
     const dataUpdate: any = { updatedAt: new Date() };
     const oldPassword = req.body.oldpassword;
     const password = req.body.password;
     const name = req.body.name;
 
-    if(oldPassword && password) {
-        const user = await db.findByEmail(email);
+    if (oldPassword && password) {
+      const user = await db.findByEmail(email);
 
-        const passwordMatch = await crypt.compare(oldPassword, user.password); 
-        if(passwordMatch) {
-          dataUpdate.password = await crypt.create(password);
-        }
-        else {
-          throw new Unauthorized('The password did not match.');
-        }
+      const passwordMatch = await crypt.compare(oldPassword, user.password);
+      if (passwordMatch) {
+        dataUpdate.password = await crypt.create(password);
+      } else {
+        throw new Unauthorized('The password did not match.');
+      }
     }
 
     name ? (dataUpdate.name = name) : {};
@@ -121,8 +119,6 @@ async function updateUser(req: Request, res: Response, next: NextFunction): Prom
   } catch (error: any) {
     next(error);
   }
-  
-
 }
 
-export { create, login, passRecovery, passUpdate, updateUser, deleteUser};
+export { create, login, passRecovery, passUpdate, updateUser, deleteUser };
